46c15746cba9d0fe22461e1f4d773f3b1559691c
yigit ocak <yigitockk@gmail.com>
2024-03-24 14:11:28 -0400

    shows page mobile styling and javascript is done

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,40 +1,85 @@
-class Person {
-    constructor(cus, pic, sik) {
-        this.cus = cus
-        this.pic = pic
-        this.sik = sik
-    }
+const ticketArray = [
+    {
+        date: "Mon Sept 09 2024",
+        venue: "Ronald Lane",
+        location: "San Francisco, CA"
+    },
+    {
 
-    shooters() {
-        console.log(`oranin tam bir ${this.pic} fiyatinda da ${this.cus} ve ${this.sik}nde 15 cm gerileme `)
+        date: "Tue Sept 17 2024",
+        venue: "Pier 3 East",
+        location: "San Francisco, CA"
+    },
+    {
+        date: "Sat Oct 12 2024",
+        venue: "View Lounge",
+        location: "San Francisco, CA"
+    },
+    {
+        date: "Sat Nov 16 202",
+        venue: "Hyatt Agency",
+        location: "San Francisco, CA"
+    },
+    {
+        date: "Fri Nov 29 2024",
+        venue: "Moscow Center",
+        location: "San Francisco, CA"
+    },
+    {
+        date: "Wed Dec 18 2024",
+        venue: "Press Club",
+        location: "San Francisco, CA"
     }
+]
+
+const shows = document.querySelector(".shows")
+
+const showsList = document.createElement("ul")
+showsList.classList.add("shows__list")
+
+const renderTickets = () => {
+    ticketArray.forEach((index) => {
+        let showsItem = document.createElement("li")
+        showsItem.classList.add("shows__item")
+        showsList.appendChild(showsItem)
+
+        let showsLabel = document.createElement("span")
+        showsLabel.classList.add("shows__label")
+        showsLabel.textContent = "DATE"
+        showsItem.appendChild(showsLabel)
+
+        let showsDate = document.createElement("p")
+        showsDate.classList.add("shows__date")
+        showsDate.textContent = index.date
+        showsItem.appendChild(showsDate)
+
+        showsLabel = document.createElement("span")
+        showsLabel.classList.add("shows__label")
+        showsLabel.textContent = "VENUE"
+        showsItem.appendChild(showsLabel)
+
+        let showsVenue = document.createElement("p")
+        showsVenue.classList.add("shows__venue")
+        showsVenue.textContent = index.venue
+        showsItem.appendChild(showsVenue)
+
+        showsLabel = document.createElement("span")
+        showsLabel.classList.add("shows__label")
+        showsLabel.textContent = "LOCATION"
+        showsItem.appendChild(showsLabel)
+
+        let showsLocation = document.createElement("p")
+        showsLocation.classList.add("shows__location")
+        showsLocation.textContent = index.location
+        showsItem.appendChild(showsLocation)
+
+        let showsButton = document.createElement("button")
+        showsButton.classList.add("shows__button")
+        showsButton.textContent = "BUY TICKETS"
+        showsItem.appendChild(showsButton)
+
+        shows.appendChild(showsList)
+    })
 }
 
-class Niggers extends Person {
-    constructor(cus, pic, sik, oran) {
-        super(cus, pic, sik);
-        this.oran = oran
-    }
-
-    shootersN() {
-        console.log(`oranin tam bir ${this.oran} fiyatinda da ${this.cus} ve ${this.sik}nde 15 cm gerileme `)
-    }
-
-}
-
-let nigers = new Niggers("z", "s", "sa", "d")
-
-console.log(nigers.shootersN)
-console.log(nigers.shooters)
-
-class Per {
-    constructor() {
-        this.yarrak = "yarrak"
-    }
-
-
-}
-
-let instance1 = new Per()
-
-console.log(instance1.yarrak)
+renderTickets()
